7a996295a8ad6543a5d8da8b6723f4ca86aa5656
Manoel Calixto <manoel.calixto.neto@gmail.com>
2025-09-07 18:45:33 -0300

    test(logger): ensure showOutput reveals channel

--- src/test/logger.showOutput.test.ts
+++ src/test/logger.showOutput.test.ts
@@ -2,28 +2,27 @@ import assert from 'assert/strict';
 import * as vscode from 'vscode';
 import { createRequire } from 'module';
 
-suite('extension deactivate', () => {
-  test('disposes logger output channel', () => {
+suite('logger showOutput', () => {
+  test('reveals output channel', () => {
     const req = createRequire(__filename);
-    let disposed = false;
+    let preserve: boolean | undefined;
     const original = vscode.window.createOutputChannel;
     (vscode.window as any).createOutputChannel = () => ({
       info: () => {},
       warn: () => {},
       error: () => {},
       trace: () => {},
-      show: () => {},
-      dispose: () => {
-        disposed = true;
-      }
+      show: (p?: boolean) => {
+        preserve = p;
+      },
+      dispose: () => {}
     });
 
-    const modPath = req.resolve('../../dist/extension');
+    const modPath = req.resolve('../utils/logger');
     delete req.cache[modPath];
-    const ext = req('../../dist/extension');
-
-    ext.deactivate();
-    assert.ok(disposed, 'disposeLogger should be invoked');
+    const { showOutput } = req('../utils/logger');
+    showOutput(true);
+    assert.strictEqual(preserve, true, 'channel.show should be invoked');
 
     (vscode.window as any).createOutputChannel = original;
   });
